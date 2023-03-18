88d807e3894bbdb54e938651f6e969c8b1e491d6
zae-park <zae_park@huinno.com>
2023-03-18 22:55:49 +0900

    add 2nd course (on doing)

---
+++ dart/course/types.dart
@@ -0,0 +1,24 @@
+import 'dart:typed_data';
+
+class arr {
+  /* Aboud 'List', 'string interpolation' and 'Collection' */
+  List<int> intergers = [0, 1, 2, 3, 4];
+  List<double> realNumbers = [0.0, 1.1, 2.2, 3.3, 4.4];
+
+  List<int> age = [15, 11, 26, 31, 9, 27];
+  List<bool> earlyBirth = [true, false, false, false, true];
+
+  List<String> user = ['A', 'B', 'C', 'D'];
+
+  void greeting(String name, int age) {
+    print('My name is $name and i\'m $age year(s) old.');   // String interpolation
+  }
+
+  void collectios() {
+    var newUser = ['Z', 'Y', 'X', for (var _user in user) _user];   // List collection
+
+    Map<String, int> userInfo = {'A': 15, 'B': 11, 'C': 26, 'D': 31}; // Map
+    print(userInfo);
+  }
+
+}
