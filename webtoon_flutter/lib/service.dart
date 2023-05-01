d86f63dd12f56e31b4ff1bd4adb57e03823e8405
zae-park <zae_park@huinno.com>
2023-05-01 19:56:41 +0900

    add webtoon Json parser

---
+++ webtoon_flutter/lib/service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'models/webtoon_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
+
+  static Future<List<WebtoonModel>> getTodayToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    } else {
+      throw Error();
+    }
+  }
+}
